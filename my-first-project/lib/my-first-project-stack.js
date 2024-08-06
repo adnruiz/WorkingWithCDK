7ce492c7ffc4ea3bf1c0071b5fcfc4d9ac18e345
Adán Ruiz Villalobos <adnruiz1@gmail.com>
2024-08-06 17:43:43 -0600

    Creating a bucket with AWS CDK

--- my-first-project/lib/my-first-project-stack.js
+++ my-first-project/lib/my-first-project-stack.js
@@ -1,5 +1,9 @@
-const { Stack, Duration } = require('aws-cdk-lib');
+const { Stack, Duration, RemovalPolicy } = require('aws-cdk-lib');
 // const sqs = require('aws-cdk-lib/aws-sqs');
+const iam = require('aws-cdk-lib/aws-iam')
+const kms = require('aws-cdk-lib/aws-kms')
+const s3 = require('aws-cdk-lib/aws-s3')
+
 
 class MyFirstProjectStack extends Stack {
   /**
@@ -12,11 +16,16 @@ class MyFirstProjectStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const createS3Bucket = new s3.Bucket(this, 'ImagesForTheRocketCode', {
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyFirstProjectQueue', {
-    //   visibilityTimeout: Duration.seconds(300)
-    // });
+    createS3Bucket.grantRead(new iam.AccountRootPrincipal());
+    console.log('Hello World')
   }
 }
 
