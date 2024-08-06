d395b4ab2e5fd664925b1f8409ec699193469de9
Adán Ruiz Villalobos <adnruiz1@gmail.com>
2024-08-06 10:09:29 -0600

    cdk first time

---
+++ my-first-project/test/my-first-project.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const MyFirstProject = require('../lib/my-first-project-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-first-project-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new MyFirstProject.MyFirstProjectStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
